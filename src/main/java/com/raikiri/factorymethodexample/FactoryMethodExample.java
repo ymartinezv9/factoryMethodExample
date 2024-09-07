afc41e8f2db619756e821612a53f858f6f897336
lutwi <lutwi@Raikiri>
2024-09-06 23:58:02 -0600

    primer comit Cambios al codigo

--- src/main/java/com/raikiri/factorymethodexample/FactoryMethodExample.java
+++ src/main/java/com/raikiri/factorymethodexample/FactoryMethodExample.java
@@ -8,9 +8,87 @@ package com.raikiri.factorymethodexample;
  *
  * @author lutwi
  */
+
+/*Se requiere de un producto o en este caso que es lo que hara el estudiante
+y este comportamiento se replicara en las distintas instancias en
+las cuales se aplique el comportamiento a la clase/objeto, este se conoce como 
+Producto
+*/
+interface Estudiante{
+    void inscribirse();
+}
+
+/*Las instancias especificas se conocen como producto concreto*/
+class EstudianteUMG implements Estudiante{
+    @Override
+    public void inscribirse(){
+        System.out.println("Se ha inscrito el estudiante en la UMG");
+    }
+}
+class EstudianteUSAC implements Estudiante{
+    @Override
+    public void inscribirse(){
+        System.out.println("Se ha inscrito el estudiante en la USAC");
+    }
+}
+class EstudianteURL implements Estudiante{
+    @Override
+    public void inscribirse(){
+        System.out.println("Se ha inscrito el estudiante en la URL");
+    }
+}
+
+/*Ahora se require de un creador el cual se encarga de generar una clase 
+abstracta la cual forzara a sobre escribir los metodos y funciones de la clase
+se le conoce como creador*/
+
+abstract class RegistroEstudiante{
+    public abstract Estudiante creatEstudiante();
+    
+    public void procesarInscripcion(){
+        Estudiante estudiante = crearEstudiante();
+        estudiante.inscribirse();
+    }
+
+    private Estudiante crearEstudiante() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
+//Ahora se generaran creadores concretos o especificos para cada nueva instancia
+class RegistroEstudiantesUMG extends RegistroEstudiante{
+
+    public Estudiante crearEstudiante(){
+        return new EstudianteUMG();   
+    }
+}
+class RegistroEstudiantesUSAC extends RegistroEstudiante{
+    @Override
+    public Estudiante crearEstudiante(){
+        return new EstudianteUSAC();   
+    }
+}
+class RegistroEstudiantesURL extends RegistroEstudiante{
+    @Override
+    public Estudiante crearEstudiante(){
+        return new EstudianteURL();   
+    }
+}
+
+
+
 public class FactoryMethodExample {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+            
+            RegistroEstudiante registro = new RegistroEstudianteUMG();
+            registro.procesarInscripcion();
+            
+            registro = new RegistroEstudianteUSAC();
+            registro.procesarInscripcion();
+            
+            registro = new RegistroEstudianteURL();
+            registro.procesarInscripcion();
     }
 }
---
+++ src/main/java/com/raikiri/factorymethodexample/RegistroEstudianteUMG.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.raikiri.factorymethodexample;
+
+/**
+ *
+ * @author lutwi
+ */
+public class RegistroEstudianteUMG extends RegistroEstudiante {
+
+    public RegistroEstudianteUMG() {
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public Estudiante creatEstudiante() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
