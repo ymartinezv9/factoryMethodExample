5acb7899582614377be3dd8bfef4614113da9d1b
lutwi <lutwi@Raikiri>
2024-09-06 22:35:20 -0600

    primer comit

---
+++ src/main/java/com/raikiri/factorymethodexample/FactoryMethodExample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.raikiri.factorymethodexample;
+
+/**
+ *
+ * @author lutwi
+ */
+public class FactoryMethodExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
